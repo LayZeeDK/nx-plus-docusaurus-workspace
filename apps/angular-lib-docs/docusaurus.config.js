5b4e641e6fab9ef784685ee50e8654272e91863e
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-02 13:22:12 +0200

    feat: add TypeDoc API reference

--- apps/angular-lib-docs/docusaurus.config.js
+++ apps/angular-lib-docs/docusaurus.config.js
@@ -7,6 +7,18 @@ module.exports = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc',
+
+      // Plugin / TypeDoc options
+      {
+        entryPoints: ['../../libs/angular-lib/src/index.ts'],
+        out: 'api', // default value
+        tsconfig: '../../libs/angular-lib/tsconfig.lib.json',
+      },
+    ],
+  ],
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
